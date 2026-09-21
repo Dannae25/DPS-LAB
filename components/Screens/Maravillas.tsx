2afa7007d69684faac1646c1da0e3c9edd2a53c3
Dannae25 <danae.g2507@gmail.com>
2026-09-21 00:35:18 -0600

    Agregar: modulo de Maravillas, incluyendo el mapa

---
+++ components/Screens/Maravillas.tsx
@@ -0,0 +1,15 @@
+import { createStackNavigator } from 'expo-router/js-stack';
+import Detalle from './DetalleMaravillas';
+import MaravillasList from './ListaMaravillas';
+import type { MaravillasStackParamList } from './types';
+
+const Stack = createStackNavigator<MaravillasStackParamList>();
+
+const Maravillas = () => (
+  <Stack.Navigator initialRouteName="Maravillas">
+    <Stack.Screen name="Maravillas" component={MaravillasList} options={{ headerShown: false }} />
+    <Stack.Screen name="Consejos" component={Detalle} options={{ headerShown: false }} />
+  </Stack.Navigator>
+);
+
+export default Maravillas;
